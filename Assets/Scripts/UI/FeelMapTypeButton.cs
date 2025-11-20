2633a7ef45690f9b1a232221bffda1105b700fa4
Poulpux <114729806+poulpux@users.noreply.github.com>
2025-11-20 19:52:54 +0100

    Pop

---
+++ Assets/Scripts/UI/FeelMapTypeButton.cs
@@ -0,0 +1,25 @@
+using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeelMapTypeButton : MonoBehaviour
+{
+    [SerializeField] MMF_Player map0, map1, map2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastManager_.I.allTag[GV.TagSO._editorMapType]._click2DEvent.AddListener(() => SetRightFeedback());
+    }
+
+    private void SetRightFeedback()
+    {
+        if (EditorManager.I.currentMapData._mapTypeC1 == 0)
+            map0.PlayFeedbacks();
+        else if (EditorManager.I.currentMapData._mapTypeC1 == 1)
+            map1.PlayFeedbacks();
+        else if (EditorManager.I.currentMapData._mapTypeC1 == 2)
+            map2.PlayFeedbacks();
+    }
+}
