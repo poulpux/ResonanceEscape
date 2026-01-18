f4ebe73d82e76980088af067d6404c8e8c2dac81
Poulpux <114729806+poulpux@users.noreply.github.com>
2026-01-18 11:44:42 +0100

    La souris géré

--- Assets/Scripts/UI/FollowMouse.cs
+++ Assets/Scripts/UI/FollowMouse.cs
@@ -7,16 +7,19 @@ using static Sirenix.OdinInspector.Editor.UnityPropertyEmitter;
 public class FollowMouse : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] GameObject trail;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameManager.I._waitingToActEvent.AddListener(() => spriteRenderer.enabled = false );
-        GameManager.I._overwatchEvent.AddListener(() => spriteRenderer.enabled = false);
-        GameManager.I._winTheLevelFeedbackEvent.AddListener(() => spriteRenderer.enabled = false);
-        GameManager.I._goToMenuEvent.AddListener(() => spriteRenderer.enabled = true);
-        MenuManager.I._endTutoEvent.AddListener(() => spriteRenderer.enabled = false);
-    }
+    bool onTuto;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    GameManager.I._waitingToActEvent.AddListener(() => { if (!onTuto) Active(false); });
+    //    GameManager.I._overwatchEvent.AddListener(() => { if (!onTuto) Active(false); });
+    //    GameManager.I._winTheLevelFeedbackEvent.AddListener(() => Active(false));
+    //    GameManager.I._goToMenuEvent.AddListener(() => { onTuto = false;  Active(true); });
+    //    MenuManager.I._startTutoEvent.AddListener(() => { print("passe"); onTuto = true; Active(true); });
+    //    MenuManager.I._endTutoEvent.AddListener(() => { onTuto = false; Active(false); });
+    //}
 
     // Update is called once per frame
     void FixedUpdate()
@@ -26,5 +29,11 @@ public class FollowMouse : MonoBehaviour
 
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
         transform.position = worldPos -9.5f * Vector3.forward;
+    }
+
+    private void Active(bool active)
+    {
+        spriteRenderer.enabled = active;
+        trail.SetActive(active);
     }
 }
